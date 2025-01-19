c184bb211690b5c7bb0182c5fb9993dd5c4d1ce6
Ayowole Ojoade <87288346+Cypher012@users.noreply.github.com>
2025-01-19 20:37:58 +0100

    Update pageTitle.tsx

--- components/pageTitle.tsx
+++ components/pageTitle.tsx
@@ -10,7 +10,7 @@ const PageTitle = ({ title, classname }: PageTitleProps) => {
   return (
     <h2
       className={clsx(
-        'text-3xl font-bold text-gray-800 dark:text-gray-100 mb-10 mt-0 md:mt-0',
+        'text-3xl font-bold text-gray-800 dark:text-gray-100 mb-10 mt-4 md:mt-0',
         classname
       )}
     >
